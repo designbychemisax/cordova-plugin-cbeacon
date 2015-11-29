469295411885b19cf557d7aa4836b6a454d6263c
José María Campaña Rojas <chemisax@gmail.com>
2015-11-29 10:52:08 +0900

    Added detection for Location Manager and Bluetooth states

--- src/ios/Cbeacon.swift
+++ src/ios/Cbeacon.swift
@@ -7,46 +7,116 @@
 
 import Foundation
 import CoreLocation
+import CoreBluetooth
 
-@objc(CBeacon) class CBeacon : CDVPlugin, CLLocationManagerDelegate {
-    
+@objc(CBeacon) class CBeacon : CDVPlugin, CLLocationManagerDelegate, CBCentralManagerDelegate {
+
+    var centralManager:CBCentralManager!
     let locationManager = CLLocationManager()
-    var callbackId = "";
-    
+    var callbackId = ""
+
+    var status : [String:Bool] = ["permission" : false, "bluetooth" : false]
+
     override func pluginInitialize () {
-        locationManager.delegate = self;
+        locationManager.delegate = self
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
     }
-    
+
+    func register (command: CDVInvokedUrlCommand) {
+        print ("\n\ncBeacon by José María Campaña Rojas,\ndesign by chemisax,\nwww.chemisax.com\n\n")
+        self.callbackId = command.callbackId
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        pluginResult.setKeepCallbackAsBool(true)
+        commandDelegate?.sendPluginResult(pluginResult, callbackId:self.callbackId)
+    }
+
+    func getBeaconStatus (command: CDVInvokedUrlCommand) {
+        print ("Requesting beacon status")
+        self.status["permission"] = CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsDictionary: self.status)
+        commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId)
+    }
+
+    func requestAuthorization (command: CDVInvokedUrlCommand) {
+        print ("Requesting authorization")
+        if CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId)
+    }
+
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        //Call callback to tell ble state changed;
+
+        switch (central.state) {
+        case .PoweredOff:
+            print("CoreBluetooth BLE hardware is powered off")
+            self.status["bluetooth"] = false
+
+        case .PoweredOn:
+            print("CoreBluetooth BLE hardware is powered on and ready")
+            self.status["bluetooth"] = true
+
+        case .Resetting:
+            print("CoreBluetooth BLE hardware is resetting")
+            self.status["bluetooth"] = false
+
+        case .Unauthorized:
+            print("CoreBluetooth BLE state is unauthorized")
+            self.status["bluetooth"] = false
+
+        case .Unknown:
+            print("CoreBluetooth BLE state is unknown")
+            self.status["bluetooth"] = false
+
+        case .Unsupported:
+            print("CoreBluetooth BLE hardware is unsupported on this platform")
+            self.status["bluetooth"] = false
+        }
+    }
+
+    //Old functions
+
     func authorizationStatusWhenInUse (command: CDVInvokedUrlCommand) {
         let permission = CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsBool: permission)
+        print("authorizationStatusWhenInUse \(permission)")
         commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId)
     }
-    
+
     func requestWhenInUseAuthorization (command: CDVInvokedUrlCommand) {
+        print ("requestWhenInUseAuthorization")
         locationManager.requestWhenInUseAuthorization()
     }
-    
+
     func startRangingBeaconsInRegion (command: CDVInvokedUrlCommand) {
         let UUID = command.arguments[0] as! String;
         let identifier = command.arguments[1] as! String;
         let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: UUID)!, identifier: identifier)
+        print ("start ranging beacons for \(identifier) (\(UUID))")
+        self.callbackId = command.callbackId
         locationManager.startRangingBeaconsInRegion(region)
     }
-    
+
     func stopRangingBeaconsInRegion (command: CDVInvokedUrlCommand) {
         let UUID = command.arguments[0] as! String;
         let identifier = command.arguments[1] as! String;
         let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: UUID)!, identifier: identifier)
         locationManager.stopRangingBeaconsInRegion(region)
     }
-    
+
     func didRangeBeacons (command: CDVInvokedUrlCommand) {
         self.callbackId = command.arguments[0] as! String;
     }
-    
+
     func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
-        print(beacons)
+        //print(beacons)
+
+        //commandDelegate?.evalJs("console.log('DidRangeBeacins!')")
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        pluginResult.setKeepCallbackAsBool(true)
+        //commandDelegate?.sendPluginResult(pluginResult, callbackId:self.callbackId)
     }
-    
+
 }
--- www/cbeacon.js
+++ www/cbeacon.js
@@ -1,21 +1,57 @@
-'use strict';  
-  
-var exec = require('cordova/exec');  
-  
-var cBeacon = {  
-  
-  authorizationStatusWhenInUse: function(onSuccess) {  
-    return exec(onSuccess, 'CBeacon', 'authorizationStatusWhenInUse', []);  
-  },
+cordova.define("cordova-plugin-cbeacon.cbeacon", function(require, exports, module) { 'use strict';
 
-  requestWhenInUseAuthorization: function() {  
-    return exec(onSuccess, onFail, 'CBeacon', 'requestWhenInUseAuthorization', []);  
-  },
+    var exec = require('cordova/exec');
 
-  startRangingBeaconsInRegion: function(UUID, identifier, didRangeBeacons) {  
-    return exec(onSuccess, onFail, 'CBeacon', 'requestWhenInUseAuthorization', []);  
-  }
-  
-};  
-  
-module.exports = cBeacon;
+    var cBeacon = {
+
+        callbacks : {
+            "didRangeBeacons" : []
+        },
+
+        errorHandler : function (error) {},
+
+        emptyHandler : function () {},
+
+        swiftAction : function (params) {
+            alert("received command from objective c");
+        },
+
+        connect: function() {
+            alert("connecting cBeacon");
+            return exec(this.swiftAction, this.errorHandler, 'CBeacon', 'register', []);
+        },
+
+        on : function (event, callback) {
+            if (this.callbacks[event]) {
+                this.callbacks[event].push(callback);
+            }
+        },
+
+        getBeaconStatus : function (callback) {
+            return exec(callback, this.errorHandler, 'CBeacon', 'getBeaconStatus', []);
+        },
+
+
+        requestAuthorization: function() {
+            return exec(this.emptyHandler, this.errorHandler, 'CBeacon', 'requestAuthorization', []);
+        },
+
+        /**
+         * Old commands
+         * @param onSuccess
+         * @returns {*}
+         */
+        authorizationStatusWhenInUse: function(onSuccess) {
+            return exec(onSuccess, 'CBeacon', 'authorizationStatusWhenInUse', []);
+        },
+
+
+
+        startRangingBeaconsInRegion: function(UUID, identifier, didRangeBeacons) {
+            return exec(onSuccess, onFail, 'CBeacon', 'requestWhenInUseAuthorization', []);
+        }
+
+    };
+
+    module.exports = cBeacon;
+});
